5ca6169aab24cd9f222bc91965690a05671567e7
dinesh-58 <86787475+dinesh-58@users.noreply.github.com>
2024-05-16 19:46:16 +0545

    fix api request

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,26 @@
 import Image from "next/image";
 
 export default function Home() {
-  console.log('testing');
   return (
-    <main>Hello</main>
+    <main>Hello
+        <ProductsShowcase />
+    </main>
   );
+}
+
+type StrapiResponse = {
+    data: [],
+    meta: {},
+}
+
+async function ProductsShowcase() {
+    const res: any = await fetch(`${process.env.STRAPI_URL}/api/books?pagination[pageSize]=4&pagination[page]=1&publicationState=live&locale[0]=en`);
+
+    const products: [] = (await res.json()).data;
+    return (
+        <section>
+            <h4>Best Selling</h4>
+            {products.map(p => <div key={p.id}>{p.attributes.title}</div>)}
+        </section>
+    );
 }
